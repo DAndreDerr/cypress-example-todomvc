d61b8eb7f460e76644fb4b308322f4cc8a10c70d
DAndreDerr <dandre020727@gmail.com>
2023-07-31 22:16:56 -0400

    Done

---
+++ cypress/e2e/testingGI.cy.js
@@ -0,0 +1,30 @@
+describe('template spec', () => {
+    beforeEach(function () {
+        cy.visit('http://localhost:8888')
+    })
+
+    it('should add three todo items', () => {
+        // add todo items
+        cy.get('.new-todo').type('Make every second count{enter}')
+        cy.get('.new-todo').type('Invest in yourself{enter}')
+        cy.get('.new-todo').type('Learn Cypress{enter}')
+
+        cy.get('.todo-list').children().should('have.length', 3)
+
+        // check if items were added
+        cy.get('.todo-count').contains('3 items left')
+        
+        cy.get('.todo-list').children().eq(0).find('label').should('contain', 'Make every second count')
+        
+        cy.get('.todo-list').children().eq(1).find('label').should('contain', 'Invest in yourself')
+        
+        cy.get('.todo-list').children().eq(2).find('label').should('contain', 'Learn Cypress')
+
+
+        // here we mark Learn Cypress Complete
+        cy.contains('.todo-list label', 'Learn Cypress').siblings('.toggle').click()
+
+        // Verify if the item is marked as completed by checking the 
+        cy.get('.todo-list').children().eq(2).should('have.class', 'completed')
+    })
+})
